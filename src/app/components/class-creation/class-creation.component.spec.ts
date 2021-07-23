88e5387f1ed9458a2855c82e948ea4751cb0e3c2
nameless <notputting@mymailhere.com>
2021-07-23 13:24:48 +0300

    Component Skeleton

---
+++ src/app/components/class-creation/class-creation.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClassCreationComponent } from './class-creation.component';
+
+describe('ClassCreationComponent', () => {
+  let component: ClassCreationComponent;
+  let fixture: ComponentFixture<ClassCreationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ClassCreationComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ClassCreationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
