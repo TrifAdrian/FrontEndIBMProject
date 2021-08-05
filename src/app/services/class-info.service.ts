ad8e25b1b9a168ae3e0484f2452dd71ee76f3a6b
Alexandru Manafu <notputting@mymailhere.com>
2021-08-05 14:12:48 +0300

    Date switching

--- src/app/services/class-info.service.ts
+++ src/app/services/class-info.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
 import { ClassMockService,Class } from './class-mock.service';
+import { DateManageService } from './date-manage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClassInfoService {
 
-  constructor(private classMock : ClassMockService) { }
+  classes : Class[] = this.mockClasses();
+  days:number[] = [1, 2, 3, 4, 5];
 
-  classes : Class[] = [];
-  date : Date = new Date();
+
+  constructor(private classMock : ClassMockService, private dateManage : DateManageService) {
+    this.refreshClasses()
+  }
+
+  public refreshClasses()
+  {
+    this.getMockClasses();
+    this.classes = this.getClassesByWeek();
+  }
+
   public target : number = 0;
 
   private getMockClasses(): void {
-    this.classMock.getArrayClasses(this.date,2)
+    this.classMock.getArrayClasses(2)
     .subscribe(classes => this.classes = classes);
   }
 
@@ -37,5 +48,45 @@ export class ClassInfoService {
   {
     this.getMockClasses();
     return this.classes;
+  }
+
+  getClassesByWeek()
+  {
+    let result : Class[] = [];
+
+    for(let i : number = 0;i<this.classes.length; i=i+1)
+    {
+      if(this.dateManage.dateInWeek(this.classes[i].date))
+          result.push(this.classes[i]);
+    }
+    return result;
+  }
+
+  getClassesByDay(day : number) : Class[]
+  {
+    let result : Class[] = [];
+    for(let i : number = 0;i<this.classes.length; i=i+1)
+    {
+      if(this.classes[i].date.getDay()==day)
+        result.push(this.classes[i]);
+    }
+    return result;
+  }
+
+  constructClassMatrix() : Class[][]
+  {
+    let result : Class[][] = [];
+    for(let i : number = 0;i<this.days.length; i=i+1)
+    {
+      result.push(this.getClassesByDay(this.days[i]));
+    }
+
+    result = this.transposeMatrix(result);
+    return result;
+  }
+
+  transposeMatrix(mat: Class[][]) 
+  {
+    return mat[0].map((_, colIndex) => mat.map(row => row[colIndex]));
   }
 }
