ff9a11e0073bd433c0fcac1c5043381fb6fe3622
DenisaSeran00 <denisa.seran00@e-utv.ro>
2021-08-09 15:45:58 +0300

    visualImprovements

--- src/app/components/class-details/class-details.component.ts
+++ src/app/components/class-details/class-details.component.ts
@@ -20,6 +20,12 @@ export class ClassDetailsComponent implements OnInit {
   role : string | null = null;
   teachers : Teacher[] = this.classInfo.teachers;
   sections : string[] = this.classInfo.sections;
+  className: string = "";
+  classYear: string="";
+  classDate: string="";
+  classStart: string="";
+  classEnd: string="";
+  classCapacity: string="";
 
   inputTeacher : string = "";
   inputSection : string = "";
@@ -27,16 +33,46 @@ export class ClassDetailsComponent implements OnInit {
   ngOnInit(): void {
     this.targetClass = this.classInfo.getTarget();
     this.role = this.userMock.getUserRole();
+    this.loadDefaulfInputs();
+  }
 
-    if(!!this.targetClass)
-    {
-      this.inputTeacher  = this.targetClass.teacher;
-      this.inputSection = this.targetClass.section;
+  loadDefaulfInputs(){
+    if(!!this.targetClass){
+        this.className=this.targetClass?.name;
+        this.classDate=this.targetClass?.getDate();
+        this.classYear=""+this.targetClass.year;
+        this.inputSection=this.targetClass.section;
+        this.inputTeacher=this.targetClass.teacher;
+        this.classCapacity=""+this.targetClass.capacity;
+        this.classEnd=this.targetClass.end;
+        this.classStart=this.targetClass.start;
+
     }
   }
 
   onSubmit()
   {
-    
+    let args = new Array();   
+      args.push(this.className, this.classYear,this.inputSection,
+        this.inputTeacher,this.classDate,this.classStart,this.classEnd);
+        
+      this.printValues(args);
+      
+      console.log(this.formValidation.validTime(this.classStart));
+      console.log(this.formValidation.checkIfDate(new Date(this.classEnd)));
+      
   }
+
+  printValues <T>(array : T[])
+  {
+    for(let i = 0; i<array.length;i=i+1)
+    {
+      console.log(array[i]);
+    }
+  }
+
+  
 }
+
+  
+
